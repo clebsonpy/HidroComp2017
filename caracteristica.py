859ca3ece67145f3c6e3bdd8e021c7bfb68ff3e7
Clebson Carvalho <clebson2007.farias@gmail.com>
2017-03-16 15:32:40 -0300

    Objeto

--- caracteristica.py
+++ caracteristica.py
@@ -5,54 +5,57 @@ Created on Wed Mar 15 22:57:24 2017
 
 @author: clebson
 """
+import os
 import pandas as pd
 import calendar as cal
 import arquivoTxt as arq
-import os
 
-#Ano hidrlogico
-def mesInicioAnoHidrologico(dados):
-    grupoMesAno = dados.groupby(pd.Grouper(freq='M')).mean().to_period()
-    indexMult = list(zip(*[grupoMesAno.index.month, grupoMesAno.index.year]))
-    indexN = pd.MultiIndex.from_tuples(indexMult, names=["Mes", "Ano"])
-    grupoMesAno.set_index(indexN, inplace=True)
-    grupoMesMedia = grupoMesAno.groupby(level='Mes').mean()
-    mesHidro = grupoMesMedia.idxmin().values
-    mesHidroAbr = cal.month_abbr[mesHidro[0]].upper()
-    return mesHidro[0], mesHidroAbr
-
-#Periodos sem falhas
-def periodoSemFalhas(ganttBool):
-    aux = []
-    listaInicio = []
-    listaFim = []
-    for i in ganttBool.index:
-        if ~ganttBool.loc[i].values:
-            aux.append(i)
-        elif len(aux) > 2 and ganttBool.loc[i].values:
+class Caracteristicas():
+    def __init__(self, dadosSeries):
+        self.dadosVazao = dadosSeries
+    #Ano hidrlogico
+    def mesInicioAnoHidrologico(self):
+        grupoMesAno = self.dadosVazao.groupby(pd.Grouper(freq='M')).mean().to_period()
+        indexMult = list(zip(*[grupoMesAno.index.month, grupoMesAno.index.year]))
+        indexN = pd.MultiIndex.from_tuples(indexMult, names=["Mes", "Ano"])
+        grupoMesAno.set_index(indexN, inplace=True)
+        grupoMesMedia = grupoMesAno.groupby(level='Mes').mean()
+        mesHidro = grupoMesMedia.idxmin().values
+        mesHidroAbr = cal.month_abbr[mesHidro[0]].upper()
+        return mesHidro[0], mesHidroAbr
+    
+    #Periodos sem falhas
+    def periodoSemFalhas(self, ganttBool):
+        aux = []
+        listaInicio = []
+        listaFim = []
+        for i in ganttBool.index:
+            if ~ganttBool.loc[i].values:
+                aux.append(i)
+            elif len(aux) > 2 and ganttBool.loc[i].values:
+                listaInicio.append(aux[0])
+                listaFim.append(aux[-1])
+                aux = []
+                
+        if len(aux) > 0:
             listaInicio.append(aux[0])
             listaFim.append(aux[-1])
-            aux = []
-            
-    if len(aux) > 0:
-        listaInicio.append(aux[0])
-        listaFim.append(aux[-1])
-    dic = {'Inicio': listaInicio, 'Fim': listaFim}
-    return pd.DataFrame(dic)
-
-def falhas(dadosVazao):
-    dadosVazao.sort_index(inplace=True)
-    nFalhas = dadosVazao.isnull().sum()
-    ganttMes = dadosVazao.isnull().groupby(pd.Grouper(freq = 'M')).sum()
-    ganttBool = dadosVazao.isnull()
-    #ganttBool.drop_duplicates(keep='last', inplace=True)
-    for i in ganttMes.index:
-        if ganttMes.loc[i].isnull().all():
-            ganttMes.set_value(index = i, col = ganttMes.axes[1], value = i.day)
-    return nFalhas, ganttBool, ganttMes.to_period()
+        dic = {'Inicio': listaInicio, 'Fim': listaFim}
+        return pd.DataFrame(dic)
+    
+    def falhas(self):
+        self.dadosVazao.sort_index(inplace=True)
+        nFalhas = self.dadosVazao.isnull().sum()
+        ganttMes = self.dadosVazao.isnull().groupby(pd.Grouper(freq = 'M')).sum()
+        ganttBool = self.dadosVazao.isnull()
+        #ganttBool.drop_duplicates(keep='last', inplace=True)
+        for i in ganttMes.index:
+            if ganttMes.loc[i].isnull().all():
+                ganttMes.set_value(index = i, col = ganttMes.axes[1], value = i.day)
+        return nFalhas, ganttBool, ganttMes.to_period()
 
 if __name__ == "__main__":
     caminho = os.getcwd()
     dados = arq.separaDadosConsisBruto(arq.trabaLinhas(caminho, '49370000'), tipo=2)
-    nfalhas, ganttBool, ganttMes = falhas(dados)
-    periodos = periodoSemFalhas(ganttBool)
+    nfalhas, ganttBool, ganttMes = Caracteristicas(dados).falhas()
+    periodos = Caracteristicas(dados).periodoSemFalhas(ganttBool)
--- graficos.py
+++ graficos.py
@@ -5,35 +5,35 @@ Created on Thu Mar 16 03:42:07 2017
 
 @author: clebson
 """
-import pandas as pd
+import os
 import arquivoTxt as arq
 import caracteristica as crt
 import prepara as pre
-import os
+import plotly.tools as tls
+import plotly.figure_factory as FF
+import plotly.offline as off
 
-def plotlyCredenciais(username, apiKey):
-    import plotly.tools as tls
-    tls.set_credentials_file(username=username, api_key= apiKey)
-    tls.set_config_file(world_readable=True, sharing='public')
-
-def plotGantt(dfGantt, filename):
-    import plotly.figure_factory as FF
-    import plotly.offline as off
-    fig = FF.create_gantt(dfGantt, index_col='IndexCol', colors = ['#000000', '#858585'], group_tasks=True, bar_width=0.475)
-    off.plot(fig, filename=filename)
-
-def plotHidro(dfg, filename):
-    import plotly.offline as off
-    off.plot(dfg, filename=filename)
-
+class Graficos():
+    
+    def plotlyCredenciais(self, username, apiKey):
+        tls.set_credentials_file(username=username, api_key= apiKey)
+        tls.set_config_file(world_readable=True, sharing='public')
+    
+    def plotGantt(self, dfGantt, filename):
+        fig = FF.create_gantt(dfGantt, index_col='IndexCol', colors = ['#000000', '#858585'], group_tasks=True, bar_width=0.475)
+        off.plot(fig, filename=filename+'.html')
+    
+    def plotHidro(self, dfg, filename):
+        off.plot(dfg, filename=filename+'.html')
+    
 if __name__ == "__main__":
 
     caminho = os.getcwd()
     dadosVazao = arq.separaDadosConsisBruto(arq.trabaLinhas(caminho, '49370000'), tipo=2)
-    nFalhas, ganttBool, ganttMes = crt.falhas(dadosVazao)
-    periodos = crt.periodoSemFalhas(ganttBool)
-    #dfGantt = pre.gantt(periodos, '49370000')
+    nFalhas, ganttBool, ganttMes = crt.Caracteristicas(dadosVazao).falhas()
+    periodos = crt.Caracteristicas(dadosVazao).periodoSemFalhas(ganttBool)
+    dfGantt = pre.gantt(periodos, '49370000')
     #plotGantt(dfGantt, filename='ganttChart')
-    mesHidro, mesHidroAbr = crt.mesInicioAnoHidrologico(dadosVazao)
+    mesHidro, mesHidroAbr = crt.Caracteristicas(dadosVazao).mesInicioAnoHidrologico()
     grupos, dfg = pre.grupoAnoHidro(dadosVazao, mesHidroAbr)
-    plotHidro(dfg, 'line-mode')
+    Graficos().plotGantt(dfGantt, filename='ganttChart')
